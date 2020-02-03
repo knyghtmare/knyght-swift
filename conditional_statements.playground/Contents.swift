a0993a5ff65d00a8d2f8990918942ccf2da520ad
knyghtmare <tahsinjahinkhalid@gmail.com>
2020-02-03 18:18:55 +0600

    Update Contents.swift

--- conditional_statements.playground/Contents.swift
+++ conditional_statements.playground/Contents.swift
@@ -1,3 +1,23 @@
 import Cocoa
 
 var str = "Hello, playground"
+
+var a = 5
+
+/*
+ || => OR
+ && => AND
+ == => equals to
+ <= => less than equal to
+ >= => greater than equal to
+ */
+
+if(a == 5) {
+    print("Only if a is 5.")
+}
+else if(a == 8) {
+    print("Only if a is 8.")
+}
+else {
+    print("a is neither 5 or 8.")
+}
