d011a06d7fde6fd0f1df640b9f3e6df8e45540d5
knyghtmare <tahsinjahinkhalid@gmail.com>
2020-02-03 18:58:41 +0600

    Update Contents.swift

--- CarClass.playground/Contents.swift
+++ CarClass.playground/Contents.swift
@@ -1,3 +1,30 @@
 import Cocoa
 
 var str = "Hello, playground"
+
+// inheritance between classes
+
+class Car {
+    var topSpeed = 200
+    
+    func drive() {
+        print("Driving at \(topSpeed) km/h")
+    }
+}
+
+class FutureCar {
+    var topSpeed = 250
+    
+    func drive() {
+        print("Driving at \(topSpeed) km/h")
+    }
+    
+    func fly() {
+        print("Flying")
+    }
+}
+
+let myCar = Car()
+myCar.drive()
+
+
