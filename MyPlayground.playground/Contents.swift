3f57acfdedcc310e4464305b59709a477950926d
knyghtmare <tahsinjahinkhalid@gmail.com>
2020-02-03 18:07:37 +0600

    Update Contents.swift

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,21 @@
 import Cocoa
 
+// in swift, variables holds data
+// the var keyword is used to declare a new
+// varaible
+
+// equal key is the assignment operator
+
 var str = "Hello, playground"
+
+// prints with a new line
+print(str)
+
+var a = 1
+var b = 2
+b = 1
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
