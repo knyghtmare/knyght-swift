101ea12fbaa8f410a70080a02c4729769708329e
knyghtmare <tahsinjahinkhalid@gmail.com>
2020-02-03 18:54:31 +0600

    Update Contents.swift

--- Classes.playground/Contents.swift
+++ Classes.playground/Contents.swift
@@ -5,5 +5,19 @@ var str = "Hello, playground"
 // classes
 
 class BlogPost {
+    var blogTitle = ""
+    var blogAuthor = ""
+    var blogContents = ""
+    var numOfComments = 0
     
+    func addComment() {
+        numOfComments += 1
+    }
 }
+
+var blogPostOne = BlogPost()
+blogPostOne.blogTitle = "The Hobbit"
+blogPostOne.blogAuthor = "Tolkien"
+blogPostOne.blogContents = "This book is a prequel to Lord of the Rings"
+blogPostOne.addComment()
+print(blogPostOne.numOfComments)
