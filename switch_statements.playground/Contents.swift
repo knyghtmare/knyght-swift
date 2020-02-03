bab23dd8c640c799e4967dc3ed914401264b0270
knyghtmare <tahsinjahinkhalid@gmail.com>
2020-02-03 18:24:52 +0600

    Update Contents.swift

--- switch_statements.playground/Contents.swift
+++ switch_statements.playground/Contents.swift
@@ -1,3 +1,20 @@
 import Cocoa
 
 var str = "Hello, playground"
+
+var grade = "A"
+
+switch grade {
+    
+    case "A":
+        print("Your grade is A.")
+        break
+    
+    case "B":
+        print("Your grade is B.")
+        break
+    
+    default:
+        print("You have not been graded.")
+        break
+}
