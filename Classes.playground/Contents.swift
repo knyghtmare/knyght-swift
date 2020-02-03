95b59336b67514f79cec20ceb8e8b66f2964ee78
knyghtmare <tahsinjahinkhalid@gmail.com>
2020-02-03 19:34:21 +0600

    Update Contents.swift

--- Classes.playground/Contents.swift
+++ Classes.playground/Contents.swift
@@ -4,20 +4,27 @@ var str = "Hello, playground"
 
 // classes
 
+class Person {
+    var name:String?
+}
+
 class BlogPost {
-    var blogTitle = ""
-    var blogAuthor = ""
-    var blogContents = ""
+    var blogTitle:String?
+    var blogAuthor:Person?
+    var blogContents:String?
     var numOfComments = 0
     
     func addComment() {
         numOfComments += 1
     }
 }
+var personAuthor = Person()
+personAuthor.name = "Tolkien"
 
 var blogPostOne = BlogPost()
 blogPostOne.blogTitle = "The Hobbit"
-blogPostOne.blogAuthor = "Tolkien"
+blogPostOne.blogAuthor = personAuthor
 blogPostOne.blogContents = "This book is a prequel to Lord of the Rings"
 blogPostOne.addComment()
 print(blogPostOne.numOfComments)
+
