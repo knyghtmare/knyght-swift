57b3008b95b3224b2b06a3130cacab499e8255d8
knyghtmare <tahsinjahinkhalid@gmail.com>
2020-02-04 16:51:40 +0600

    added template for myFirstApp

---
+++ myFirstApp/myFirstApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  myFirstApp
+//
+//  Created by Tahsin Jahin Khalid on 04/02/2020.
+//  Copyright © 2020 Tahsin Jahin Khalid. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ myFirstApp/myFirstAppTests/myFirstAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  myFirstAppTests.swift
+//  myFirstAppTests
+//
+//  Created by Tahsin Jahin Khalid on 04/02/2020.
+//  Copyright © 2020 Tahsin Jahin Khalid. All rights reserved.
+//
+
+import XCTest
+@testable import myFirstApp
+
+class myFirstAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
