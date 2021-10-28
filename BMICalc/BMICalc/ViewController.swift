bf4a519379fb905108918c27736d6e7570057d54
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2021-10-29 02:56:46 +0600

    updated

--- BMICalc/BMICalc/ViewController.swift
+++ BMICalc/BMICalc/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weightVal: UILabel!
+    
+    @IBOutlet weak var heightVal: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func weightChange(_ sender: UISlider) {
+        // change weight value
+        let weight = Int(sender.value)
+        weightVal.text = String(weight)
+    }
+    
+    @IBAction func heightChange(_ sender: UISlider) {
+        let height = String(format: "%.2f", sender.value)
+        heightVal.text = height
+    }
+    
 
 }
 
