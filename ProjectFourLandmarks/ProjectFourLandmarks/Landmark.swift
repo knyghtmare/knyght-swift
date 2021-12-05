e2b3d256ef8fc54a8c24cd69d0d50b622e86ea1a
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2021-12-06 05:30:53 +0600

    Create Landmark.swift

---
+++ ProjectFourLandmarks/ProjectFourLandmarks/Landmark.swift
@@ -0,0 +1,20 @@
+//
+//  Landmark.swift
+//  ProjectFourLandmarks
+//
+//  Created by Tahsin Jahin Khalid on 6/12/21.
+//
+
+import SwiftUI
+
+struct Landmark: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Landmark_Previews: PreviewProvider {
+    static var previews: some View {
+        Landmark()
+    }
+}
