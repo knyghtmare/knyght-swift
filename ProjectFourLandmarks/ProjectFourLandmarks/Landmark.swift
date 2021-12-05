54fb2898bbac2f9fa92e9785c886d5d91b950268
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2021-12-06 05:35:13 +0600

    Update Landmark.swift

--- ProjectFourLandmarks/ProjectFourLandmarks/Landmark.swift
+++ ProjectFourLandmarks/ProjectFourLandmarks/Landmark.swift
@@ -5,16 +5,31 @@
 //  Created by Tahsin Jahin Khalid on 6/12/21.
 //
 
+import Foundation
 import SwiftUI
+import CoreLocation
 
-struct Landmark: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+        var image: Image {
+            Image(imageName)
+        }
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
     }
-}
 
-struct Landmark_Previews: PreviewProvider {
-    static var previews: some View {
-        Landmark()
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
     }
 }
