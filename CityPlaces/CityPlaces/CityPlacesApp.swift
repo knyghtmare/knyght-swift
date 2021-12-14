a982ed76fa8cf2c7c30f2e97e28a8bfcb62a34e9
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2021-12-14 19:27:57 +0600

    started new project

---
+++ CityPlaces/CityPlaces/CityPlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityPlacesApp.swift
+//  CityPlaces
+//
+//  Created by Tahsin Jahin Khalid on 14/12/21.
+//
+
+import SwiftUI
+
+@main
+struct CityPlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CityPlaces/CityPlaces/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CityPlaces
+//
+//  Created by Tahsin Jahin Khalid on 14/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
