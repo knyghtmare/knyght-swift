9055e33d81dfb347512b4d860edd6a0ee7264d2b
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2021-12-14 20:11:26 +0600

    updated code

--- CityPlaces/CityPlaces/ContentView.swift
+++ CityPlaces/CityPlaces/ContentView.swift
@@ -9,18 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Orchard Point")
-                .font(.title)
-            HStack {
-                Text(/*@START_MENU_TOKEN@*/"Dhanmondi"/*@END_MENU_TOKEN@*/)
-                    .font(.subheadline)
-                Spacer()
-                Text(/*@START_MENU_TOKEN@*/"Dhaka City"/*@END_MENU_TOKEN@*/)
-                    .font(.subheadline)
+        VStack {
+            MapView().frame(height: 300).ignoresSafeArea(edges: .top)
+            // trying to fit the imageview along the boundary
+            // of mapview and the text
+            ImageView().offset(y: -105).padding(.bottom, -200)
+            // info text
+            VStack(alignment: .leading) {
+                Text("Orchard Point")
+                    .font(.title)
+                HStack {
+                    Text(/*@START_MENU_TOKEN@*/"Dhanmondi"/*@END_MENU_TOKEN@*/)
+                        .font(.subheadline)
+                    Spacer()
+                    Text(/*@START_MENU_TOKEN@*/"Dhaka City"/*@END_MENU_TOKEN@*/)
+                        .font(.subheadline)
+                }
             }
+            .padding(.all)
+            // push to the top of screen
+            Spacer()
         }
-        .padding(.all)
             
     }
 }
--- CityPlaces/CityPlaces/ImageView.swift
+++ CityPlaces/CityPlaces/ImageView.swift
@@ -10,6 +10,11 @@ import SwiftUI
 struct ImageView: View {
     var body: some View {
         Image("orchardpoint")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
     }
 }
 
