737e248c1dcd7e759ce3336624a5b10ff46528ba
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2021-12-14 20:31:33 +0600

    fixed code

---
+++ CityPlaces/CityPlaces/Views/LandmarkRow.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkRow.swift
+//  CityPlaces
+//
+//  Created by Tahsin Jahin Khalid on 14/12/21.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow()
+    }
+}
