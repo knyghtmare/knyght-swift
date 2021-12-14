3f517c9f649098d34d3400f8421705c12eeab7f8
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2021-12-15 00:58:40 +0600

    Update LandmarkRow.swift

--- CityPlaces/CityPlaces/Views/LandmarkRow.swift
+++ CityPlaces/CityPlaces/Views/LandmarkRow.swift
@@ -18,10 +18,10 @@ struct LandmarkRow: View {
             Text(landmark.name)
             Spacer()
             
-            if landmark.isFavorite {
-                Image(systemName: "star.fill")
-                    .foregroundColor(.yellow)
-            }
+            //if landmark.isFavorite {
+            //    Image(systemName: "star.fill")
+            //        .foregroundColor(.yellow)
+            //}
         }
     }
 }
