b4ef7f55eb2419e7a8641bc4842411edc4354461
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2021-12-16 02:39:59 +0600

    Create WeatherRow.swift

---
+++ WeatherDemo/WeatherDemo/Components/WeatherRow.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherRow.swift
+//  WeatherDemo
+//
+//  Created by Tahsin Jahin Khalid on 16/12/21.
+//
+
+import SwiftUI
+
+struct WeatherRow: View {
+    var logo: String
+    var name: String
+    var value: String
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeatherRow_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherRow()
+    }
+}
