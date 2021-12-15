203d0d47023b2f6eb61a0768952f791ace9d87ab
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2021-12-16 03:35:28 +0600

    updated code

---
+++ CityPlaces/CityPlaces/Views/Badge/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  CityPlaces
+//
+//  Created by Tahsin Jahin Khalid on 16/12/21.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
