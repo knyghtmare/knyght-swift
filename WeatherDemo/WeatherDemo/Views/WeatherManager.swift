3cc085c50e87e8893f7e37e3044dffc02d42d7cc
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2021-12-17 18:08:36 +0600

    hid the API key

--- WeatherDemo/WeatherDemo/Views/WeatherManager.swift
+++ WeatherDemo/WeatherDemo/Views/WeatherManager.swift
@@ -11,7 +11,7 @@ import CoreLocation
 class WeatherManager {
     func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws -> ResponseBody {
         // code goes here
-        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=bda843b075d912954f864056ad795a18&units=metric") else { fatalError("Missing URL") }
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=MYSTUPIDAPIKEY&units=metric") else { fatalError("Missing URL") }
         
         let urlRequest = URLRequest(url: url)
         
