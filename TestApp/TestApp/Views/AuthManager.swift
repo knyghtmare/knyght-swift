ff066ec9de929f9fbec58c05867d92db6ea5beb4
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2021-12-21 02:48:56 +0600

    Create AuthManager.swift

---
+++ TestApp/TestApp/Views/AuthManager.swift
@@ -0,0 +1,13 @@
+//
+//  AuthManager.swift
+//  TestApp
+//
+//  Created by Tahsin Jahin Khalid on 21/12/21.
+//
+
+import Foundation
+import LocalAuthentication
+
+class AuthManager: ObservableObject {
+    private(set) var context = LAContext()
+}
