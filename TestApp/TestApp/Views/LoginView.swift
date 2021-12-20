f501a22b44b7d3043b218d0534b5ec5c0f5fc79c
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2021-12-21 02:56:09 +0600

    Update LoginView.swift

--- TestApp/TestApp/Views/LoginView.swift
+++ TestApp/TestApp/Views/LoginView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct LoginView: View {
+    @EnvironmentObject var authManager: AuthManager
+    
     var body: some View {
         VStack(spacing: 40) {
             Title()
@@ -21,6 +23,6 @@ struct LoginView: View {
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
-        LoginView()
+        LoginView().environmentObject(AuthManager())
     }
 }
