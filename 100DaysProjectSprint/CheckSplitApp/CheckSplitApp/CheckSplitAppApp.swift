4a76f1dfe678bfad5852fe483bfc45aa45713a94
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-02-04 18:05:31 +0600

    add new base project

---
+++ 100DaysProjectSprint/CheckSplitApp/CheckSplitApp/CheckSplitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckSplitAppApp.swift
+//  CheckSplitApp
+//
+//  Created by Tahsin Jahin Khalid on 4/2/22.
+//
+
+import SwiftUI
+
+@main
+struct CheckSplitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 100DaysProjectSprint/CheckSplitApp/CheckSplitApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CheckSplitApp
+//
+//  Created by Tahsin Jahin Khalid on 4/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Welcome to the CheckSplitApp!")
+            .padding(30)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
