d43dc4ffd55afafbe2aa989912c9a2f0afb48124
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-02-05 19:51:37 +0600

    Update ContentView.swift

--- 100DaysProjectSprint/CheckSplitApp/CheckSplitApp/ContentView.swift
+++ 100DaysProjectSprint/CheckSplitApp/CheckSplitApp/ContentView.swift
@@ -8,28 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    /* In the case of storing simple program state like the
+     number of times a button was tapped, we can use a
+     property wrapper from SwiftUI called @State, like this:
+     */
+    @State private var tapCount = 0
+    
+    @State private var name = ""
+    
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Harry"
+    
     var body: some View {
         NavigationView {
             Form {
                 Section {
-                    Text("Hello, world!")
-                    Text("Hello, world!")
+                    TextField("Enter your Name", text: $name)
+                    Text("Your name is \(name)")
                 }
                 Section {
-                    Text("Hello, world!")
-                    Text("Hello, world!")
+                    Button("Tap Count: \(tapCount)") {
+                        self.tapCount += 1
+                    }
                 }
-                Section {
-                    Text("Hello, world!")
-                    Text("Hello, world!")
+                ForEach(0..<100) {
+                    Text("Row \($0)")
                 }
-                Section {
-                    Text("Hello, world!")
-                    Text("Hello, world!")
-                }
-                Section {
-                    Text("Hello, world!")
-                    Text("Hello, world!")
+                Picker("Select your student", selection: $selectedStudent) {
+                    ForEach(students, id: \.self) {
+                        Text($0)
+                    }
                 }
                 Section {
                     Text("Hello, world!")
@@ -40,6 +48,8 @@ struct ContentView: View {
                     Text("Hello, world!")
                 }
             }
+            .navigationTitle("CheckSplit")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
