1b941ccd7f80d3421a1498c16a837061a0381e8b
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-02-06 08:14:19 +0600

    create new view

---
+++ 100DaysProjectSprint/SpeculateFlags/SpeculateFlags/AlertTestView.swift
@@ -0,0 +1,20 @@
+//
+//  AlertTestView.swift
+//  SpeculateFlags
+//
+//  Created by Tahsin Jahin Khalid on 6/2/22.
+//
+
+import SwiftUI
+
+struct AlertTestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AlertTestView_Previews: PreviewProvider {
+    static var previews: some View {
+        AlertTestView()
+    }
+}
