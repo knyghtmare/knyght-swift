b0197cae287dad69b0f2a8f2cda14a18ac76b19b
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-02-06 08:12:21 +0600

    Update ButtonTestView.swift

--- 100DaysProjectSprint/SpeculateFlags/SpeculateFlags/ButtonTestView.swift
+++ 100DaysProjectSprint/SpeculateFlags/SpeculateFlags/ButtonTestView.swift
@@ -9,12 +9,39 @@ import SwiftUI
 
 struct ButtonTestView: View {
     var body: some View {
-        Button("Delete selection", role: .destructive, action: executeDelete)
-            //.buttonStyle(.borderedProminent)
-            .buttonStyle(.bordered)
-        Button("Button 3") { }
-            .buttonStyle(.borderedProminent)
-            .tint(.mint)
+        VStack(spacing: 20) {
+        HStack(spacing: 20) {
+            Button("Delete selection", role: .destructive, action: executeDelete)
+                //.buttonStyle(.borderedProminent)
+                .buttonStyle(.bordered)
+            Button("Button 3") { }
+                .buttonStyle(.borderedProminent)
+                .tint(.mint)
+        }
+        HStack {
+            // custom button
+            Button {
+                print("Button was tapped")
+            } label: {
+                Text("Tap me!")
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(.red)
+            }
+            // button with image + text
+            Button {
+                print("Edit button was tapped")
+            } label: {
+                Label("Edit", systemImage: "pencil")
+            }
+            // image only button
+            Button {
+                print("Edit button was tapped")
+            } label: {
+                Image(systemName: "pencil")
+            }
+        }
+        }
     }
     
     func executeDelete() {
