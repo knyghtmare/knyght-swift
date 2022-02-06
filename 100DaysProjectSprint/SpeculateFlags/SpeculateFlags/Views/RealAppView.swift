ee756c6b5fd68dd2eac5f1165ce304a9de6df83f
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-02-06 10:59:05 +0600

    Update RealAppView.swift

--- 100DaysProjectSprint/SpeculateFlags/SpeculateFlags/Views/RealAppView.swift
+++ 100DaysProjectSprint/SpeculateFlags/SpeculateFlags/Views/RealAppView.swift
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct RealAppView: View {
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
-    var correctAnswer = Int.random(in: 0...2)
+    // array of countries which are shuffled each time
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    // how to pick correct answer
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    // show alert
+    @State private var showingScore = false
+    // this property to store the title that will be shown inside the alert:
+    @State private var scoreTitle = ""
     
     var body: some View {
-        VStack {
-            Text("Tap the flag of")
-            Text(countries[correctAnswer])
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .purple]), startPoint: .bottom, endPoint: .top)
+                .ignoresSafeArea()
+            VStack(spacing: 30) {
+                VStack(spacing: 20) {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                }
+                ForEach(0..<3) { number in
+                    Button {
+                       // flag was tapped
+                        flagTapped(number)
+                    } label: {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                    }
+                }
+            }
+            .padding(40)
+            //.background(.ultraThinMaterial)
         }
+    }
+    
+    // function to check if right/wrong
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+
+        showingScore = true
+    }
+    
+    // repeat after correct answer is given for a country
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
