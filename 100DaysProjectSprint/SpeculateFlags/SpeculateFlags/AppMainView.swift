8c5a38575f60c9fb5244ed7c2932040175d8e21f
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-02-06 06:53:54 +0600

    lay ground work for project 2

---
+++ 100DaysProjectSprint/SpeculateFlags/SpeculateFlags/AppMainView.swift
@@ -0,0 +1,20 @@
+//
+//  AppMainView.swift
+//  SpeculateFlags
+//
+//  Created by Tahsin Jahin Khalid on 6/2/22.
+//
+
+import SwiftUI
+
+struct AppMainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppMainView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppMainView()
+    }
+}
---
+++ 100DaysProjectSprint/SpeculateFlags/SpeculateFlags/SpeculateFlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeculateFlagsApp.swift
+//  SpeculateFlags
+//
+//  Created by Tahsin Jahin Khalid on 6/2/22.
+//
+
+import SwiftUI
+
+@main
+struct SpeculateFlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
