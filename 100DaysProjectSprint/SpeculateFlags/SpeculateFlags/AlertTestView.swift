d35610059f618f3cc2b8ecf3e39b56e9d01679a4
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-02-06 08:20:29 +0600

    Update AlertTestView.swift

--- 100DaysProjectSprint/SpeculateFlags/SpeculateFlags/AlertTestView.swift
+++ 100DaysProjectSprint/SpeculateFlags/SpeculateFlags/AlertTestView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct AlertTestView: View {
+    // variable to check if alert is on/off
+    @State private var showingAlert = false
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
