71a5d8b46ff4e7c2ea668b050ea3415df79460e8
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-02-06 08:04:24 +0600

    Create ButtonTestView.swift

---
+++ 100DaysProjectSprint/SpeculateFlags/SpeculateFlags/ButtonTestView.swift
@@ -0,0 +1,26 @@
+//
+//  ButtonTestView.swift
+//  SpeculateFlags
+//
+//  Created by Tahsin Jahin Khalid on 6/2/22.
+//
+
+import SwiftUI
+
+struct ButtonTestView: View {
+    var body: some View {
+        Button("Delete selection", role: .destructive, action: executeDelete)
+            //.buttonStyle(.borderedProminent)
+            .buttonStyle(.bordered)
+    }
+    
+    func executeDelete() {
+        print("Deleting...")
+    }
+}
+
+struct ButtonTestView_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonTestView()
+    }
+}
