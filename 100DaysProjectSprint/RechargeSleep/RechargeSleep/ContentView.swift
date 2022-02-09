7549474ca6cce62dae067c96c31781b88267c2e1
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-02-09 16:30:42 +0600

    Update ContentView.swift

--- 100DaysProjectSprint/RechargeSleep/RechargeSleep/ContentView.swift
+++ 100DaysProjectSprint/RechargeSleep/RechargeSleep/ContentView.swift
@@ -13,16 +13,33 @@ struct ContentView: View {
     // var to assign a date
     @State private var wakeUp = Date.now
     
+    /*
+    let components = Calendar.current.dateComponents([.hour, .minute], from: someDate)
+    let hour = components.hour ?? 0
+    let minute = components.minute ?? 0
+    */
+    
     var body: some View {
         // Text("Hello, world!")
         //    .padding()
         // step is set to 0.25 which is 25% of an hour
         // that is, it's 15 minutes
-        VStack {
-            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-            DatePicker("Please enter a date", selection: $wakeUp)
-        }
+        //VStack {
+            Form {
+                Section {
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                    DatePicker("Please enter timr", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                    DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
+                }
+                Section {
+                    Text(Date.now.formatted(date: .long, time: .shortened))
+                }
+            }
+        //}
         .padding(20)
+        .background(.white)
+        //.ignoresSafeArea()
     }
 }
 
