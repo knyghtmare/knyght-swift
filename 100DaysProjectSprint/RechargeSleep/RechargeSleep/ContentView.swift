8cc85e50296aefbb0fcd60c14f8c1997ef78c0c5
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-02-09 14:55:41 +0600

    Update ContentView.swift

--- 100DaysProjectSprint/RechargeSleep/RechargeSleep/ContentView.swift
+++ 100DaysProjectSprint/RechargeSleep/RechargeSleep/ContentView.swift
@@ -8,13 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    // var to keep track of number of sleep hours
     @State private var sleepAmount = 8.0
+    // var to assign a date
+    @State private var wakeUp = Date.now
     
     var body: some View {
         // Text("Hello, world!")
         //    .padding()
-        Stepper("\(sleepAmount) hours", value: $sleepAmount)
-            .padding()
+        // step is set to 0.25 which is 25% of an hour
+        // that is, it's 15 minutes
+        VStack {
+            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+            DatePicker("Please enter a date", selection: $wakeUp)
+        }
+        .padding(20)
     }
 }
 
