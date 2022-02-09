c85cada2101d634e51fec8d154e5723855bb1854
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-02-09 09:25:01 +0600

    Create TestButtonView.swift

---
+++ TestApp/TestApp/Views/TestButtonView.swift
@@ -0,0 +1,22 @@
+//
+//  TestButtonView.swift
+//  TestApp
+//
+//  Created by Tahsin Jahin Khalid on 9/2/22.
+//
+
+import SwiftUI
+
+struct TestButtonView: View {
+    var body: some View {
+        Text("Hello")
+            .frame(width: 200, height: 200)
+            .background(.red)
+    }
+}
+
+struct TestButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestButtonView()
+    }
+}
