ff757cfa1552895f237e05e18a5e27eb857ca637
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-02-22 05:23:56 +0600

    Update FruitData.swift

--- 100DaysProjectSprint/FructusApp/FructusApp/Data/FruitData.swift
+++ 100DaysProjectSprint/FructusApp/FructusApp/Data/FruitData.swift
@@ -5,4 +5,10 @@
 //  Created by Tahsin Jahin Khalid on 22/2/22.
 //
 
-import 
+import SwiftUI
+
+// MARK: FRUITS DATA
+
+let fruitsData: [Fruit] = [
+    Fruit(title: <#T##String#>, headline: <#T##String#>, image: <#T##String#>, gradientColors: <#T##[Color]#>, description: <#T##String#>, nutrition: <#T##[String]#>)
+]
