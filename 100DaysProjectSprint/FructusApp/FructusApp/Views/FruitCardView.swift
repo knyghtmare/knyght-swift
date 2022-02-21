40123d9fe17916fedbd8106b585d350fc4bae38f
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-02-22 04:36:31 +0600

    Update FruitCardView.swift

--- 100DaysProjectSprint/FructusApp/FructusApp/Views/FruitCardView.swift
+++ 100DaysProjectSprint/FructusApp/FructusApp/Views/FruitCardView.swift
@@ -8,13 +8,25 @@
 import SwiftUI
 
 struct FruitCardView: View {
+    // MARK: - Properties
+    
+    // MARK: - Body
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            // Fruit: Image
+            // Fruit: Title
+            Text("Blueberry")
+            // Fruit: Headline
+            // Button: Start
+        } //: VStack
     }
 }
+
+// MARK: - Preview
 
 struct FruitCardView_Previews: PreviewProvider {
     static var previews: some View {
         FruitCardView()
+            .previewLayout(.fixed(width: 320, height: 640))
     }
 }
