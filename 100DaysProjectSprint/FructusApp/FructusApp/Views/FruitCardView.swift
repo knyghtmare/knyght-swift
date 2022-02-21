d435707fa7010aa83ed63e8b1723edbd03c2ac51
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-02-22 04:46:25 +0600

    Update FruitCardView.swift

--- 100DaysProjectSprint/FructusApp/FructusApp/Views/FruitCardView.swift
+++ 100DaysProjectSprint/FructusApp/FructusApp/Views/FruitCardView.swift
@@ -12,13 +12,27 @@ struct FruitCardView: View {
     
     // MARK: - Body
     var body: some View {
-        VStack(spacing: 20) {
-            // Fruit: Image
-            // Fruit: Title
-            Text("Blueberry")
-            // Fruit: Headline
-            // Button: Start
-        } //: VStack
+        ZStack {
+            VStack(spacing: 20) {
+                // Fruit: Image
+                Image("blueberry")
+                    .resizable()
+                    .scaledToFit()
+                    // add shadow
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
+                // Fruit: Title
+                Text("Blueberry")
+                    .foregroundColor(Color.white)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .shadow(color: Color(red: 0, blue: 0, green: 0, opacity: 0.15), radius: 2, x: 2, y: 2)
+                // Fruit: Headline
+                // Button: Start
+            } //: VStack
+        } //: ZStack
+        .frame(minWidth: 0, idealWidth: 320, maxWidth: .infinity, minHeight: 0, idealHeight: 640, maxHeight: .infinity, alignment: .center)
+        .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")]), startPoint: .top, endPoint: .bottom))
+        .cornerRadius(20)
     }
 }
 
