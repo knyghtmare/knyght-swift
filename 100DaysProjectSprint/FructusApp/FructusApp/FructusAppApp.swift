c0b73cad8cd7d2bfbca160260e458f14d07c0254
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-02-22 04:13:51 +0600

    setup project 4

---
+++ 100DaysProjectSprint/FructusApp/FructusApp/FructusAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusAppApp.swift
+//  FructusApp
+//
+//  Created by Tahsin Jahin Khalid on 22/2/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
