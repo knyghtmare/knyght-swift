4b80e57cfe190912d99ab4ac8a198859d7f05d59
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-02-22 14:07:26 +0600

    Update FructusAppApp.swift

--- 100DaysProjectSprint/FructusApp/FructusApp/App/FructusAppApp.swift
+++ 100DaysProjectSprint/FructusApp/FructusApp/App/FructusAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct FructusAppApp: App {
     
     //newly introduced wrapper, usually store a small piece of information
-    @AppStorage("isOnboarding") var isOnboarding = true
+    @AppStorage("isOnboarding") var isOnboarding : Bool = true
     
     var body: some Scene {
         WindowGroup {
