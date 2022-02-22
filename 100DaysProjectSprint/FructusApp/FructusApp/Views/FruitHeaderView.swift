f0102d6a38805397636e71f4f8cf5ee92fe03c02
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-02-22 13:40:21 +0600

    Create FruitHeaderView.swift

---
+++ 100DaysProjectSprint/FructusApp/FructusApp/Views/FruitHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  FruitHeaderView.swift
+//  FructusApp
+//
+//  Created by Tahsin Jahin Khalid on 22/2/22.
+//
+
+import SwiftUI
+
+struct FruitHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FruitHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitHeaderView()
+    }
+}
