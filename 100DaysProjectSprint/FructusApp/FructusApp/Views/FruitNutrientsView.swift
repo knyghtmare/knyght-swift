81d0942212446e96765a0eb56fd58591fee1b2b5
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-02-22 21:29:36 +0600

    Update FruitNutrientsView.swift

--- 100DaysProjectSprint/FructusApp/FructusApp/Views/FruitNutrientsView.swift
+++ 100DaysProjectSprint/FructusApp/FructusApp/Views/FruitNutrientsView.swift
@@ -10,9 +10,30 @@ import SwiftUI
 struct FruitNutrientsView: View {
     // MARK: Properties
     
+    var fruit: Fruit
+    let nutrients: [String] = ["Energy","Sugar","Fat","Protein","Vitamains","Minerals"]
+    
     // MARK: BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GroupBox() {
+            DisclosureGroup("Nutrional Value per 100g") {
+                ForEach(0 ..< nutrients.count, id: \.self) { item in
+                    Divider().padding(.vertical, 2)
+                    
+                    HStack {
+                        Group {
+                            Image(systemName: "info.circle")
+                            Text(nutrients[item])
+                        }
+                        .foregroundColor(fruit.gradientColors[1])
+                        .font(.system(.body).bold())
+                        Spacer(minLength: 25)
+                        Text(fruit.nutrition[item])
+                            .multilineTextAlignment(.trailing)
+                    } //: HSTACK
+                } //: LOOP
+            } //: GROUP
+        } //: BOX
     }
 }
 
@@ -20,6 +41,9 @@ struct FruitNutrientsView: View {
 
 struct FruitNutrientsView_Previews: PreviewProvider {
     static var previews: some View {
-        FruitNutrientsView()
+        FruitNutrientsView(fruit: fruitsData[9])
+            .preferredColorScheme(.light)
+            .previewLayout(.fixed(width: 375, height: 480))
+            .padding()
     }
 }
