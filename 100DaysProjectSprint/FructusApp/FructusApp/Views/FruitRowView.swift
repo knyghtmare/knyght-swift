1027844e9ad2b048b97b8f42c7e5b5a19d7a2ad9
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-02-22 06:32:16 +0600

    Update FruitRowView.swift

--- 100DaysProjectSprint/FructusApp/FructusApp/Views/FruitRowView.swift
+++ 100DaysProjectSprint/FructusApp/FructusApp/Views/FruitRowView.swift
@@ -23,11 +23,13 @@ struct FruitRowView: View {
                 .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.30), radius: 3, x: 2, y: 2)
                 .background(LinearGradient(gradient: Gradient(colors: fruit.gradientColors), startPoint: .top, endPoint: .bottom))
                 .cornerRadius(8)
-            VStack {
+            VStack(alignment: .leading, spacing: 5) {
                 Text(fruit.title)
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
+                    .font(.title2)
+                    .fontWeight(.bold)
                 Text(fruit.headline)
+                    .font(.caption)
+                    .foregroundColor(Color.secondary)
             } //: VStack
         } //: HStack
     }
