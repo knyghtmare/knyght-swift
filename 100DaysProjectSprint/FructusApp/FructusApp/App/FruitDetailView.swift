97b6419e027393bbc2bfffb1d03fe5b9fb6ea205
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-02-22 12:51:03 +0600

    Create FruitDetailView.swift

---
+++ 100DaysProjectSprint/FructusApp/FructusApp/App/FruitDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  FruitDetailView.swift
+//  FructusApp
+//
+//  Created by Tahsin Jahin Khalid on 22/2/22.
+//
+
+import SwiftUI
+
+struct FruitDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FruitDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitDetailView()
+    }
+}
