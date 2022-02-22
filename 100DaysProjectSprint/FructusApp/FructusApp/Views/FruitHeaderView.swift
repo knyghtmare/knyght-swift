f2fe48c5e806a05743a3915fcb38a408aca5e157
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-02-22 13:47:01 +0600

    Update FruitHeaderView.swift

--- 100DaysProjectSprint/FructusApp/FructusApp/Views/FruitHeaderView.swift
+++ 100DaysProjectSprint/FructusApp/FructusApp/Views/FruitHeaderView.swift
@@ -8,13 +8,29 @@
 import SwiftUI
 
 struct FruitHeaderView: View {
+    // MARK: - Properties
+    
+    var fruit: Fruit
+    
+    // MARK: - BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        //Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: fruit.gradientColors), startPoint: .topLeading, endPoint: .bottomTrailing)
+            Image(fruit.image)
+                .resizable()
+                .scaledToFit()
+                .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
+                .padding(.vertical, 20)
+        } //: ZSTACK
+        .frame(height: 440)
     }
 }
 
+// MARK: - PREVIEW
+
 struct FruitHeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        FruitHeaderView()
+        FruitHeaderView(fruit: fruitsData[1]).previewLayout(.fixed(width: 375, height: 440))
     }
 }
