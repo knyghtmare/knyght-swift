d8a9dcb6d0e8f87805e39d33285396cd0c8a6c6c
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-02-22 20:51:47 +0600

    Create FruitNutrientsView.swift

---
+++ 100DaysProjectSprint/FructusApp/FructusApp/Views/FruitNutrientsView.swift
@@ -0,0 +1,25 @@
+//
+//  FruitNutrientsView.swift
+//  FructusApp
+//
+//  Created by Tahsin Jahin Khalid on 22/2/22.
+//
+
+import SwiftUI
+
+struct FruitNutrientsView: View {
+    // MARK: Properties
+    
+    // MARK: BODY
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: PREVIEW
+
+struct FruitNutrientsView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitNutrientsView()
+    }
+}
