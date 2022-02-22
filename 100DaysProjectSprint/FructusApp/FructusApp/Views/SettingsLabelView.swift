9d8048aa1de651044da925ead722ed440291376c
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-02-22 22:47:36 +0600

    Create SettingsLabelView.swift

---
+++ 100DaysProjectSprint/FructusApp/FructusApp/Views/SettingsLabelView.swift
@@ -0,0 +1,26 @@
+//
+//  SettingsLabelView.swift
+//  FructusApp
+//
+//  Created by Tahsin Jahin Khalid on 22/2/22.
+//
+
+import SwiftUI
+
+struct SettingsLabelView: View {
+    // MARK: Properties
+    
+    // MARK: BODY
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: PREVIEW
+
+struct SettingsLabelView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsLabelView()
+            .previewLayout(.sizeThatFits)
+    }
+}
