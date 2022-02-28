bd7dd864bfc7d6213c7df435db9b8809e52ae501
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-02-28 21:08:53 +0600

    Create MapView.swift

---
+++ 100DaysProjectSprint/AfricaApp/AfricaApp/Views/MapView.swift
@@ -0,0 +1,25 @@
+//
+//  MapView.swift
+//  AfricaApp
+//
+//  Created by Tahsin Jahin Khalid on 28/2/22.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
