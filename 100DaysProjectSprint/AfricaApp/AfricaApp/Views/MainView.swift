15405c618b9cbddd164dad3fe0b3214bd977e890
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-02-28 21:08:54 +0600

    Create MainView.swift

---
+++ 100DaysProjectSprint/AfricaApp/AfricaApp/Views/MainView.swift
@@ -0,0 +1,25 @@
+//
+//  MainView.swift
+//  AfricaApp
+//
+//  Created by Tahsin Jahin Khalid on 28/2/22.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
