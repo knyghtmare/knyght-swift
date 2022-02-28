3ed0971a36f6c7503543442816f36e44e34ec9e0
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-02-28 21:08:56 +0600

    Create GalleryView.swift

---
+++ 100DaysProjectSprint/AfricaApp/AfricaApp/Views/GalleryView.swift
@@ -0,0 +1,23 @@
+//
+//  GalleryView.swift
+//  AfricaApp
+//
+//  Created by Tahsin Jahin Khalid on 28/2/22.
+//
+
+import SwiftUI
+
+struct GalleryView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GalleryView_Previews: PreviewProvider {
+    static var previews: some View {
+        GalleryView()
+    }
+}
