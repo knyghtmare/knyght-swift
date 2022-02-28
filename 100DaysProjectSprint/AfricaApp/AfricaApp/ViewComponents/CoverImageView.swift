e93d2b9a0a29aed4b6d18a5659c83ed845c4259d
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-01 00:33:39 +0600

    Create CoverImageView.swift

---
+++ 100DaysProjectSprint/AfricaApp/AfricaApp/ViewComponents/CoverImageView.swift
@@ -0,0 +1,34 @@
+//
+//  CoverImageView.swift
+//  AfricaApp
+//
+//  Created by Tahsin Jahin Khalid on 28/2/22.
+//
+
+import SwiftUI
+
+struct CoverImageView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        // Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+                Image("cover-lion")
+                    .resizable()
+                .scaledToFit()
+            } //: LOOP
+        } //: TAB
+        .tabViewStyle(PageTabViewStyle())
+    }
+}
+
+// MARK: - PREVIEW
+
+struct CoverImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoverImageView()
+            .previewLayout(.fixed(width: 400, height: 300))
+    }
+}
