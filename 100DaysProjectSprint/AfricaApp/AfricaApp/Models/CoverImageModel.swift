3d8c011a97830f89b8fa8d3dd4ddd78d18363002
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-01 00:33:36 +0600

    Create CoverImageModel.swift

---
+++ 100DaysProjectSprint/AfricaApp/AfricaApp/Models/CoverImageModel.swift
@@ -0,0 +1,15 @@
+//
+//  CoverImageModel.swift
+//  AfricaApp
+//
+//  Created by Tahsin Jahin Khalid on 1/3/22.
+//
+
+import SwiftUI
+
+
+struct CoverImage: Codable, Identifiable {
+    // code here
+    let id: Int
+    let name: String
+}
