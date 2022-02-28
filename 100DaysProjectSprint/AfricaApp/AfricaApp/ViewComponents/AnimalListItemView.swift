49a058be3c83e5163d6a657266bbb0f26f0570c7
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-01 02:13:50 +0600

    Create AnimalListItemView.swift

---
+++ 100DaysProjectSprint/AfricaApp/AfricaApp/ViewComponents/AnimalListItemView.swift
@@ -0,0 +1,25 @@
+//
+//  AnimalListItemView.swift
+//  AfricaApp
+//
+//  Created by Tahsin Jahin Khalid on 1/3/22.
+//
+
+import SwiftUI
+
+struct AnimalListItemView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+
+struct AnimalListItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimalListItemView()
+    }
+}
