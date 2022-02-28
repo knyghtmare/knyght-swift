a074e35ff655fc135008dd40b8060129de4ab083
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-01 01:51:51 +0600

    Create CodableBundleExtension.swift

---
+++ 100DaysProjectSprint/AfricaApp/AfricaApp/Extra/CodableBundleExtension.swift
@@ -0,0 +1,35 @@
+//
+//  CodableBundleExtension.swift
+//  AfricaApp
+//
+//  Created by Tahsin Jahin Khalid on 1/3/22.
+//
+
+import Foundation
+
+extension Bundle {
+    func decode(_ file: String) -> [CoverImage] {
+        // 1. Locate JSON file
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle!")
+        }
+        
+        // 2. create property for the data
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle.")
+        }
+        
+        // 3. Create the Decoder
+        let decoder = JSONDecoder()
+        
+        // 4. Create property for the decoded data
+        guard let loaded = try? decoder.decode([CoverImage].self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle.")
+        }
+        
+        // 5. Return ready-to-use data
+        return loaded
+    }
+}
+
+
