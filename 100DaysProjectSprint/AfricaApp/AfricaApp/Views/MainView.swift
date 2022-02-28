9083657b379135af6aba1cf7e82794654166e1ee
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-02-28 21:16:25 +0600

    Update MainView.swift

--- 100DaysProjectSprint/AfricaApp/AfricaApp/Views/MainView.swift
+++ 100DaysProjectSprint/AfricaApp/AfricaApp/Views/MainView.swift
@@ -18,7 +18,25 @@ struct MainView: View {
                     Image(systemName: "square.grid.2x2")
                     Text("Browse")
                 }
-        }
+            
+            VideoListView()
+                .tabItem {
+                    Image(systemName: "play.rectangle")
+                    Text("Watch")
+                }
+            
+            MapView()
+                .tabItem {
+                    Image(systemName: "map")
+                    Text("Locations")
+                }
+            
+            GalleryView()
+                .tabItem {
+                    Image(systemName: "photo")
+                    Text("Gallery")
+                }
+        } //: TAB
     }
 }
 
