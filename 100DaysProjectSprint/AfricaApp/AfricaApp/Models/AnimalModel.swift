fdfeaf49be918e2fe0c0b5865853019341f56936
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-01 02:28:58 +0600

    Update AnimalModel.swift

--- 100DaysProjectSprint/AfricaApp/AfricaApp/Models/AnimalModel.swift
+++ 100DaysProjectSprint/AfricaApp/AfricaApp/Models/AnimalModel.swift
@@ -5,4 +5,16 @@
 //  Created by Tahsin Jahin Khalid on 1/3/22.
 //
 
-import Foundation
+import SwiftUI
+
+struct AnimalModel: Codable, Identifiable {
+    // code here
+    let id: String
+    let name: String
+    let headline: String
+    let description: String
+    let link: String
+    let image: String
+    let gallery: [String]
+    let fact: [String]
+}
