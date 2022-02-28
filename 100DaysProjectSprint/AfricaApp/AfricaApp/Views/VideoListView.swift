739c2d5168ab95e2a5573e29ce8413a51bfb1306
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-02-28 21:08:51 +0600

    Create VideoListView.swift

---
+++ 100DaysProjectSprint/AfricaApp/AfricaApp/Views/VideoListView.swift
@@ -0,0 +1,25 @@
+//
+//  VideoListView.swift
+//  AfricaApp
+//
+//  Created by Tahsin Jahin Khalid on 28/2/22.
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+
+struct VideoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListView()
+    }
+}
