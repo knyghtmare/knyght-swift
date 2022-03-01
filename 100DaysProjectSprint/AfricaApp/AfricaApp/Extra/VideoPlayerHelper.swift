f6bfebaceeefae1261675f3eda297443f7d95bf0
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-02 01:21:54 +0600

    Update VideoPlayerHelper.swift

--- 100DaysProjectSprint/AfricaApp/AfricaApp/Extra/VideoPlayerHelper.swift
+++ 100DaysProjectSprint/AfricaApp/AfricaApp/Extra/VideoPlayerHelper.swift
@@ -8,4 +8,12 @@
 import SwiftUI
 import AVKit
 
+var videoPlayer: AVPlayer?
 
+func playVideo(fileName: String, fileFormat: String) -> AVPlayer {
+    if Bundle.main.url(forResource: fileName, withExtension: fileFormat) != nil {
+        videoPlayer = AVPlayer(url: Bundle.main.url(forResource: fileName, withExtension: fileFormat)!)
+        videoPlayer?.play()
+    }
+    return videoPlayer!
+}
