4a19398c99f82a19357d8b725dab081c4138f06b
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-02 01:31:39 +0600

    Update VideoListView.swift

--- 100DaysProjectSprint/AfricaApp/AfricaApp/Views/VideoListView.swift
+++ 100DaysProjectSprint/AfricaApp/AfricaApp/Views/VideoListView.swift
@@ -18,8 +18,10 @@ struct VideoListView: View {
     var body: some View {
         NavigationView {
             List(videos) { item in
-                VideoListItemView(video: item)
-                    .padding(.vertical, 8)
+                NavigationLink(destination: VideoPlayerView(videoSelected: item.id, videoTitle: item.name)) {
+                    VideoListItemView(video: item)
+                        .padding(.vertical, 8)
+                } //: NavLINK
             } //: List
             .listStyle(InsetGroupedListStyle())
             .navigationBarTitle("Videos", displayMode: .inline)
