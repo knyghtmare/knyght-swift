405b053bb705f8d7c6afc9c403477ca21236e0dc
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-02 00:08:01 +0600

    Update VideoListItem.swift

--- 100DaysProjectSprint/AfricaApp/AfricaApp/ViewComponents/VideoListItem.swift
+++ 100DaysProjectSprint/AfricaApp/AfricaApp/ViewComponents/VideoListItem.swift
@@ -15,12 +15,33 @@ struct VideoListItem: View {
     // MARK: - BODY
     var body: some View {
         //Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        Image("video-\(video.id)")
-            .resizable()
-            .scaledToFit()
-            .frame(height: 80)
-            .cornerRadius(9)
-        
+        HStack(spacing: 10) {
+            ZStack {
+                Image(video.thumbnail)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 80)
+                    .cornerRadius(9)
+                
+                Image(systemName: "play.circle")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 32)
+                    .shadow(radius: 4)
+            } //: ZSTACK
+            
+            VStack(alignment: .leading, spacing: 10) {
+                Text(video.name)
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                    .foregroundColor(.accentColor)
+                
+                Text(video.headline)
+                    .font(.footnote)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(2)
+            } //: VSTACK
+        } //: HSTACK
     }
 }
 
