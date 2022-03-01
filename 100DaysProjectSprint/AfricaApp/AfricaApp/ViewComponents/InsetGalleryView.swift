3133d2df92f7a2e09233627e3ad5e5b88bfa0d02
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-01 18:28:23 +0600

    Update InsetGalleryView.swift

--- 100DaysProjectSprint/AfricaApp/AfricaApp/ViewComponents/InsetGalleryView.swift
+++ 100DaysProjectSprint/AfricaApp/AfricaApp/ViewComponents/InsetGalleryView.swift
@@ -10,17 +10,31 @@ import SwiftUI
 struct InsetGalleryView: View {
     // MARK: - PROPERTIES
     
+    let animal: AnimalModel
+    
     // MARK: - BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(alignment: .center, spacing: 15) {
+                ForEach(animal.gallery, id: \.self) { item in
+                    Image(item)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 200)
+                    .cornerRadius(12)
+                }
+            } //: HSTACK
+        } //: SCROLL
     }
 }
 
 // MARK: - PREVIEW
 
 struct InsetGalleryView_Previews: PreviewProvider {
+    static let animals: [AnimalModel] = Bundle.main.decode("animals.json")
+    
     static var previews: some View {
-        InsetGalleryView()
+        InsetGalleryView(animal: animals[2])
             .previewLayout(.sizeThatFits)
             .padding()
     }
