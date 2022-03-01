a41241f85d3f578d7f7bb83fb879df9d8255c5e2
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-01 20:49:57 +0600

    Create VideoModel.swift

---
+++ 100DaysProjectSprint/AfricaApp/AfricaApp/Models/VideoModel.swift
@@ -0,0 +1,14 @@
+//
+//  VideoModel.swift
+//  AfricaApp
+//
+//  Created by Tahsin Jahin Khalid on 1/3/22.
+//
+
+import Foundation
+
+struct VideoModel: Codable, Identifiable {
+    let id: String
+    let name: String
+    let headline: String
+}
