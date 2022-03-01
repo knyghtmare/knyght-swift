a688401ffc0d7624acdf182c545053734863c0d8
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-02 01:46:05 +0600

    Update MapView.swift

--- 100DaysProjectSprint/AfricaApp/AfricaApp/Views/MapView.swift
+++ 100DaysProjectSprint/AfricaApp/AfricaApp/Views/MapView.swift
@@ -6,9 +6,17 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
     // MARK: - PROPERTIES
+    @State private var region: MKCoordinateRegion = {
+        var mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+        var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+        var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+        
+        return mapRegion
+    }()
     
     // MARK: - BODY
     var body: some View {
