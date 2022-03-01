a6790f22d67570853307cdd5559fbecd33a3983b
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-01 20:50:55 +0600

    Create VideoListItem.swift

---
+++ 100DaysProjectSprint/AfricaApp/AfricaApp/ViewComponents/VideoListItem.swift
@@ -0,0 +1,20 @@
+//
+//  VideoListItem.swift
+//  AfricaApp
+//
+//  Created by Tahsin Jahin Khalid on 1/3/22.
+//
+
+import SwiftUI
+
+struct VideoListItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VideoListItem_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListItem()
+    }
+}
