bc5d7fd7d6fd541a3f0e7670c6ce51b498b72f31
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-01 20:06:30 +0600

    Create ExternalWebLinkView.swift

---
+++ 100DaysProjectSprint/AfricaApp/AfricaApp/ViewComponents/ExternalWebLinkView.swift
@@ -0,0 +1,27 @@
+//
+//  ExternalWebLinkView.swift
+//  AfricaApp
+//
+//  Created by Tahsin Jahin Khalid on 1/3/22.
+//
+
+import SwiftUI
+
+struct ExternalWebLinkView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+
+struct ExternalWebLinkView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExternalWebLinkView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
