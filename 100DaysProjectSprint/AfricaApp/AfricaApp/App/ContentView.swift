b450bc7c8f13793f9ec8874cca1ec776b12f645f
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-01 16:10:03 +0600

    Update ContentView.swift

--- 100DaysProjectSprint/AfricaApp/AfricaApp/App/ContentView.swift
+++ 100DaysProjectSprint/AfricaApp/AfricaApp/App/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     // MARK: - PROPERTIES
     
+    let animals: [AnimalModel] = Bundle.main.decode("animals.json")
+    
     // MARK: - BODY
     var body: some View {
         NavigationView {
@@ -17,6 +19,10 @@ struct ContentView: View {
                 CoverImageView()
                     .frame(height: 300)
                     .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0 ))
+                
+                ForEach(animals) { animal in
+                    AnimalListItemView(animal: animal)
+                } //: LOOP
             } //: List
             .navigationBarTitle("Welcome to Africa", displayMode: .large)
         } //: Navigation View
@@ -26,6 +32,7 @@ struct ContentView: View {
 // MARK: - PREVIEW
 
 struct ContentView_Previews: PreviewProvider {
+    
     static var previews: some View {
         ContentView()
     }
