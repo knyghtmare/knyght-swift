f050ca9295378fce3f087d9002319303785de49c
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-01 19:50:28 +0600

    Create InsetMapView.swift

---
+++ 100DaysProjectSprint/AfricaApp/AfricaApp/ViewComponents/InsetMapView.swift
@@ -0,0 +1,28 @@
+//
+//  InsetMapView.swift
+//  AfricaApp
+//
+//  Created by Tahsin Jahin Khalid on 1/3/22.
+//
+
+import SwiftUI
+import MapKit
+
+struct InsetMapView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+
+struct InsetMapView_Previews: PreviewProvider {
+    static var previews: some View {
+        InsetMapView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
