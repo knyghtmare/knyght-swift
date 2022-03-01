3a9b22014374f9840c606a57c524b2d26a3b810a
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-01 18:20:43 +0600

    Create InsetGalleryView.swift

---
+++ 100DaysProjectSprint/AfricaApp/AfricaApp/ViewComponents/InsetGalleryView.swift
@@ -0,0 +1,20 @@
+//
+//  InsetGalleryView.swift
+//  AfricaApp
+//
+//  Created by Tahsin Jahin Khalid on 1/3/22.
+//
+
+import SwiftUI
+
+struct InsetGalleryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InsetGalleryView_Previews: PreviewProvider {
+    static var previews: some View {
+        InsetGalleryView()
+    }
+}
