887b0e75a3a0651fa1495c6b84f3d1dddb28435e
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-01 16:31:41 +0600

    Create HeadingView.swift

---
+++ 100DaysProjectSprint/AfricaApp/AfricaApp/Views/HeadingView.swift
@@ -0,0 +1,26 @@
+//
+//  HeadingView.swift
+//  AfricaApp
+//
+//  Created by Tahsin Jahin Khalid on 1/3/22.
+//
+
+import SwiftUI
+
+struct HeadingView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+
+struct HeadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeadingView()
+            .previewLayout(.sizeThatFits)
+    }
+}
