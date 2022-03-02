7f832e42aef9e4b64d5ab41a0d2ee158a0de8cef
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-02 21:12:29 +0600

    Create AnimalGridItemView.swift

---
+++ 100DaysProjectSprint/AfricaApp/AfricaApp/ViewComponents/AnimalGridItemView.swift
@@ -0,0 +1,25 @@
+//
+//  AnimalGridItemView.swift
+//  AfricaApp
+//
+//  Created by Tahsin Jahin Khalid on 2/3/22.
+//
+
+import SwiftUI
+
+struct AnimalGridItemView: View {
+    // MARK: - Properties
+    
+    // MARK: - BODY
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+
+struct AnimalGridItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimalGridItemView()
+    }
+}
