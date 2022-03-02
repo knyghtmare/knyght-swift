a94db7b39be8823fe915f0d02112d70be4e21cf7
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-02 19:56:12 +0600

    Update ContentView.swift

--- 100DaysProjectSprint/AfricaApp/AfricaApp/App/ContentView.swift
+++ 100DaysProjectSprint/AfricaApp/AfricaApp/App/ContentView.swift
@@ -19,17 +19,21 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             Group {
-                List {
-                    CoverImageView()
-                        .frame(height: 300)
-                        .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0 ))
-                    
-                    ForEach(animals) { animal in
-                        NavigationLink(destination: AnimalDetailView(animal: animal)) {
-                            AnimalListItemView(animal: animal)
-                        } // NavLINK
-                    } //: LOOP
-                } //: List
+                if !isGridViewActive {
+                    List {
+                        CoverImageView()
+                            .frame(height: 300)
+                            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0 ))
+                        
+                        ForEach(animals) { animal in
+                            NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                                AnimalListItemView(animal: animal)
+                            } // NavLINK
+                        } //: LOOP
+                    } //: List
+                } else {
+                    /*@START_MENU_TOKEN@*/EmptyView()/*@END_MENU_TOKEN@*/
+                } //: Conditional
             } //: GROUP
             .navigationBarTitle("Welcome to Africa", displayMode: .large)
             .toolbar {
