c2b7d4494cb0227cc380e7974e1a8334531d1295
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-02 21:42:44 +0600

    Update AnimalGridItemView.swift

--- 100DaysProjectSprint/AfricaApp/AfricaApp/ViewComponents/AnimalGridItemView.swift
+++ 100DaysProjectSprint/AfricaApp/AfricaApp/ViewComponents/AnimalGridItemView.swift
@@ -10,16 +10,28 @@ import SwiftUI
 struct AnimalGridItemView: View {
     // MARK: - Properties
     
+    let animal: AnimalModel
+    
     // MARK: - BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(animal.image)
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(12)
+        } //: VSTACK
     }
 }
 
 // MARK: - PREVIEW
 
 struct AnimalGridItemView_Previews: PreviewProvider {
+    static var animal: [AnimalModel] = Bundle.main.decode("animals.json")
+    
     static var previews: some View {
-        AnimalGridItemView()
+        AnimalGridItemView(animal: animal[2])
+            .previewLayout(.sizeThatFits)
+            .padding()
     }
 }
