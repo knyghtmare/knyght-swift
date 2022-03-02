6bb0075a7dc6975982268788949ba9581082e151
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-02 18:26:45 +0600

    Update GalleryView.swift

--- 100DaysProjectSprint/AfricaApp/AfricaApp/Views/GalleryView.swift
+++ 100DaysProjectSprint/AfricaApp/AfricaApp/Views/GalleryView.swift
@@ -11,6 +11,8 @@ import MapKit
 struct GalleryView: View {
     // MARK: - PROPERTIES
     
+    @State private var selectedAnimal: String = "lion"
+    
     let animals: [AnimalModel] = Bundle.main.decode("animals.json")
     
     let gridLayout: [GridItem] = [
@@ -22,16 +24,19 @@ struct GalleryView: View {
     // MARK: - BODY
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            // MARK: - GRID
-            LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
-                ForEach(animals) { item in
-                    //Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                    Image(item.image)
-                        .resizable()
-                        .scaledToFit()
-                        .clipShape(Circle())
-                }
-            } //: GRID
+            VStack {
+                // MARK: - GRID
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        //Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                    }
+                } //: GRID
+            } //: VSTACK
         } //: SCROLL
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
