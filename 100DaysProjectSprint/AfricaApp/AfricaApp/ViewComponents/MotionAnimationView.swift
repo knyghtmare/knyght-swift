df8932216d9c0f980a072eabbe72e88ddad6d7b1
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-02 06:30:56 +0600

    Update MotionAnimationView.swift

--- 100DaysProjectSprint/AfricaApp/AfricaApp/ViewComponents/MotionAnimationView.swift
+++ 100DaysProjectSprint/AfricaApp/AfricaApp/ViewComponents/MotionAnimationView.swift
@@ -10,16 +10,34 @@ import SwiftUI
 struct MotionAnimationView: View {
     // MARK: - PROPERTIES
     
+    @State private var randomCircle = Int.random(in: 12...16)
+    
+    // MARK: - FUNCTIONS
+    
+    // 1. RANDOM COORDINATE
+    func randomCoordinate(max: CGFloat) -> CGFloat {
+        return CGFloat.random(in: 0...max)
+    }
+    // 2. RANDOM SIZE
+    // 3. RANDOM SCALE
+    // 4. RANDOM SPEED
+    // 5. RANDOM DELAY
+    
     // MARK: - BODY
     var body: some View {
         GeometryReader { geometry in
             // Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
             ZStack {
-                Circle()
-                    .foregroundColor(.gray)
-                    .opacity(0.15)
-                    .frame(width: 256, height: 256, alignment: .center)
-                    .position(x: geometry.size.width/2, y: geometry.size.height/2)
+                ForEach(0...randomCircle, id: \.self) { item in
+                    Circle()
+                        .foregroundColor(.gray)
+                        .opacity(0.15)
+                        .frame(width: 256, height: 256, alignment: .center)
+                        .position(
+                            x: geometry.size.width/2,
+                            y: geometry.size.height/2
+                    )
+                } //: LOOP
                 
                 Text("Width: \(Int(geometry.size.width)) Height: \(Int(geometry.size.height))")
             } //: ZSTACK
