4c87f6a075dd144f64abf07b1558a1fac6b65e3b
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-02 06:26:19 +0600

    Update MotionAnimationView.swift

--- 100DaysProjectSprint/AfricaApp/AfricaApp/ViewComponents/MotionAnimationView.swift
+++ 100DaysProjectSprint/AfricaApp/AfricaApp/ViewComponents/MotionAnimationView.swift
@@ -15,6 +15,12 @@ struct MotionAnimationView: View {
         GeometryReader { geometry in
             // Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
             ZStack {
+                Circle()
+                    .foregroundColor(.gray)
+                    .opacity(0.15)
+                    .frame(width: 256, height: 256, alignment: .center)
+                    .position(x: geometry.size.width/2, y: geometry.size.height/2)
+                
                 Text("Width: \(Int(geometry.size.width)) Height: \(Int(geometry.size.height))")
             } //: ZSTACK
         } //: Geometry
