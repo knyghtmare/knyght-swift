90d8d9e5d9854e41775e80b82944a6d632166c82
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-03 20:39:17 +0600

    Update ContentView.swift

--- 100DaysProjectSprint/AfricaApp/AfricaApp/App/ContentView.swift
+++ 100DaysProjectSprint/AfricaApp/AfricaApp/App/ContentView.swift
@@ -53,6 +53,8 @@ struct ContentView: View {
                                 AnimalListItemView(animal: animal)
                             } // NavLINK
                         } //: LOOP
+                        
+                        CreditsView()
                     } //: List
                 } else {
                     //Text("Dummy")
