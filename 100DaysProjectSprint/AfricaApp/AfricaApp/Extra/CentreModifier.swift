34fd769c3c19a0d2c503954c64a9931ad34c321b
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-03 20:42:44 +0600

    Update CentreModifier.swift

--- 100DaysProjectSprint/AfricaApp/AfricaApp/Extra/CentreModifier.swift
+++ 100DaysProjectSprint/AfricaApp/AfricaApp/Extra/CentreModifier.swift
@@ -6,3 +6,14 @@
 //
 
 import SwiftUI
+
+struct CenterModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        HStack {
+            Spacer()
+            content
+            Spacer()
+        } //: HSTACK
+    }
+}
+
