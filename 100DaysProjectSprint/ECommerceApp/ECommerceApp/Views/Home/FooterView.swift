2d3e0008c23575c39c013f70e1d0fadc39441d29
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-07 22:06:37 +0600

    Update FooterView.swift

--- 100DaysProjectSprint/ECommerceApp/ECommerceApp/Views/Home/FooterView.swift
+++ 100DaysProjectSprint/ECommerceApp/ECommerceApp/Views/Home/FooterView.swift
@@ -16,5 +16,7 @@ struct FooterView: View {
 struct FooterView_Previews: PreviewProvider {
     static var previews: some View {
         FooterView()
+            .previewLayout(.sizeThatFits)
+            .background(ColorBackground)
     }
 }
