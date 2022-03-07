0745a9a501cad5ee9fb2a79b4386e654e3312cc2
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-07 21:28:37 +0600

    started project 6

---
+++ 100DaysProjectSprint/ECommerceApp/ECommerceApp/ECommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECommerceAppApp.swift
+//  ECommerceApp
+//
+//  Created by Tahsin Jahin Khalid on 7/3/22.
+//
+
+import SwiftUI
+
+@main
+struct ECommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
