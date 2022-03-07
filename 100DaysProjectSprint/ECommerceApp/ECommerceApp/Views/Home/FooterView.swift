5743720bcfe0b51a506f01573dcba2dfa508aaa0
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-07 22:18:08 +0600

    Update FooterView.swift

--- 100DaysProjectSprint/ECommerceApp/ECommerceApp/Views/Home/FooterView.swift
+++ 100DaysProjectSprint/ECommerceApp/ECommerceApp/Views/Home/FooterView.swift
@@ -6,12 +6,29 @@
 //
 
 import SwiftUI
+// MARK: - PROPERTIES
 
+// MARK: - BODY
 struct FooterView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("We offer the most cutting edge, comfortable, lightweight and durable football helmets in the market at affordable prices.")
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+            Image("logo-lineal")
+                .renderingMode(.template)
+                .foregroundColor(.gray)
+            Text("Copyright © Tahsin Jahin Khalid 2022\nAll Rights Reserved")
+                .font(.footnote)
+                .fontWeight(.bold)
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .layoutPriority(1)
+        } //: VSTACK
     }
 }
+
+// MARK: - PREVIEW
 
 struct FooterView_Previews: PreviewProvider {
     static var previews: some View {
