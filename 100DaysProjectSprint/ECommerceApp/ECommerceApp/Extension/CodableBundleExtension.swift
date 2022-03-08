95393d4f260a14a44086112b939537350ec842c1
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-08 21:18:30 +0600

    Update CodableBundleExtension.swift

--- 100DaysProjectSprint/ECommerceApp/ECommerceApp/Extension/CodableBundleExtension.swift
+++ 100DaysProjectSprint/ECommerceApp/ECommerceApp/Extension/CodableBundleExtension.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+extension Bundle {
+    func decode<T: Codable>(_ file: String) -> T {
+        // 1. Locate JSON file
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle!")
+        }
+        
+        // 2. create property for the data
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle.")
+        }
+        
+        // 3. Create the Decoder
+        let decoder = JSONDecoder()
+        
+        // 4. Create property for the decoded data
+        guard let loaded = try? decoder.decode(T.self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle.")
+        }
+        
+        // 5. Return ready-to-use data
+        return loaded
+    }
+}
