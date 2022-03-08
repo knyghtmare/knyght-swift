f48ca86b02dd142e04c96416ad8413117149ace3
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-08 21:20:46 +0600

    Update Brands.swift

--- 100DaysProjectSprint/ECommerceApp/ECommerceApp/DataModels/Brands.swift
+++ 100DaysProjectSprint/ECommerceApp/ECommerceApp/DataModels/Brands.swift
@@ -5,4 +5,10 @@
 //  Created by Tahsin Jahin Khalid on 8/3/22.
 //
 
-import Foundation
+import SwiftUI
+
+struct Brands: Codable, Identifiable {
+    // code here
+    let id: Int
+    let image: String
+}
