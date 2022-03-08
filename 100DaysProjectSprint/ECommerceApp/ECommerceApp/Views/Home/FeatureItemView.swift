67a9c8942e729d25b7d295a13b911da4604c6ac7
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-08 23:15:08 +0600

    Create FeatureItemView.swift

---
+++ 100DaysProjectSprint/ECommerceApp/ECommerceApp/Views/Home/FeatureItemView.swift
@@ -0,0 +1,23 @@
+//
+//  FeatureItemView.swift
+//  ECommerceApp
+//
+//  Created by Tahsin Jahin Khalid on 8/3/22.
+//
+
+import SwiftUI
+
+struct FeatureItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeatureItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeatureItemView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+            .background(ColorBackground)
+    }
+}
