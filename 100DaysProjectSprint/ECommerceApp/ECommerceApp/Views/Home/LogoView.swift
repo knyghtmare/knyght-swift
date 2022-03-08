60aedc8a7285838cfc705a69deacc67a9da2adb0
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-08 19:47:27 +0600

    Update LogoView.swift

--- 100DaysProjectSprint/ECommerceApp/ECommerceApp/Views/Home/LogoView.swift
+++ 100DaysProjectSprint/ECommerceApp/ECommerceApp/Views/Home/LogoView.swift
@@ -13,7 +13,18 @@ struct LogoView: View {
     // MARK: - BODY<##><##>
     var body: some View {
         HStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Text("Touch".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+            Image("logo-dark")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30, alignment: .center)
+            Text("Down".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
         } //:HSTACK
     }
 }
