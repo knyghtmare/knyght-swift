76689a193b30c4899eb5b0d86df488f7a4449901
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-08 21:02:12 +0600

    Update ContentView.swift

--- 100DaysProjectSprint/ECommerceApp/ECommerceApp/App/ContentView.swift
+++ 100DaysProjectSprint/ECommerceApp/ECommerceApp/App/ContentView.swift
@@ -14,6 +14,11 @@ struct ContentView: View {
     var body: some View {
         //Text("Hello, world!")
         VStack(spacing: 0) {
+            NavBarView()
+                .padding()
+                .background(Color.white)
+                .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
+            Spacer()
             FooterView()
                 .padding(.horizontal)
         } //: VSTACK
