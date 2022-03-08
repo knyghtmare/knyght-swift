9108d2183564ce617de4eb0b51f9385462a91bfe
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-08 21:25:17 +0600

    Update Product.swift

--- 100DaysProjectSprint/ECommerceApp/ECommerceApp/DataModels/Product.swift
+++ 100DaysProjectSprint/ECommerceApp/ECommerceApp/DataModels/Product.swift
@@ -5,4 +5,14 @@
 //  Created by Tahsin Jahin Khalid on 8/3/22.
 //
 
-import Foundation
+import SwiftUI
+
+struct Product: Codable, Identifiable {
+    // code here
+    let id: Int
+    let name: String
+    let image: String
+    let price: Int
+    let description: String
+    let color: [Double]
+}
