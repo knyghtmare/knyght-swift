66b84ba93e132ace27d348c9a1fa7d80c384f5e8
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-09 02:55:08 +0600

    Update CategoryItemView.swift

--- 100DaysProjectSprint/ECommerceApp/ECommerceApp/Views/Home/CategoryItemView.swift
+++ 100DaysProjectSprint/ECommerceApp/ECommerceApp/Views/Home/CategoryItemView.swift
@@ -20,8 +20,19 @@ struct CategoryItemView: View {
                     .resizable()
                     .scaledToFit()
                     .frame(width: 30, height: 30, alignment: .center)
-                Text("Press ME")
+                    .foregroundColor(.gray)
+                Text(category.name.uppercased())
+                    .fontWeight(.light)
+                    .foregroundColor(.gray)
+                
+                Spacer()
             } //HSTACK
+            .padding()
+            .background(Color.white.cornerRadius(12))
+            .background(
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(Color.gray, lineWidth: 1)
+            )
         }) //BUTTON
     }
 }
