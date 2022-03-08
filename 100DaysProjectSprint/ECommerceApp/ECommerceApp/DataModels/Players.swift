5243e64cb1fa820639effec1ce2c18b5ab459de0
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-08 21:16:23 +0600

    Update Players.swift

--- 100DaysProjectSprint/ECommerceApp/ECommerceApp/DataModels/Players.swift
+++ 100DaysProjectSprint/ECommerceApp/ECommerceApp/DataModels/Players.swift
@@ -5,4 +5,10 @@
 //  Created by Tahsin Jahin Khalid on 8/3/22.
 //
 
-import Foundation
+// import Foundation
+import SwiftUI
+
+struct Players: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
