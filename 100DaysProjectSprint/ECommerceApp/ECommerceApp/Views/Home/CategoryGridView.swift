4cf3da94b2a87f14a6eb1c766ecf047b34e197b8
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-09 17:38:30 +0600

    Create CategoryGridView.swift

---
+++ 100DaysProjectSprint/ECommerceApp/ECommerceApp/Views/Home/CategoryGridView.swift
@@ -0,0 +1,36 @@
+//
+//  CategoryGridView.swift
+//  ECommerceApp
+//
+//  Created by Tahsin Jahin Khalid on 9/3/22.
+//
+
+import SwiftUI
+
+struct CategoryGridView: View {
+    // MARK: - Properties
+    
+    // MARK: - BODY<##><##>
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false, content: {
+            LazyHGrid(rows: gridLayout, alignment: .center, spacing: columnSpacing, pinnedViews: [], content: {
+                Text("Placeholder")
+                Text("Placeholder")
+            }) // GRID
+                .frame(height: 140)
+                .padding(.horizontal, 15)
+                .padding(.vertical, 10)
+        }) //Scroll
+    }
+}
+
+// MARK: - PREVIEW<##>
+
+struct CategoryGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryGridView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+            .background(ColorBackground)
+    }
+}
