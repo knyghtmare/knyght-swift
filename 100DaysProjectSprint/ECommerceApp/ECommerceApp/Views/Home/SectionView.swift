a4ee1cc9fa580c2b5cc13cd1b2d7e5d82c4334af
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-09 17:53:41 +0600

    Update SectionView.swift

--- 100DaysProjectSprint/ECommerceApp/ECommerceApp/Views/Home/SectionView.swift
+++ 100DaysProjectSprint/ECommerceApp/ECommerceApp/Views/Home/SectionView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct SectionView: View {
+    // MARK: - PROPERTIES
+    
+    @State var rotateClockwise: Bool
+    
+    // MARK: - BODY<##><##>
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            Text("Categories".uppercased())
+                .font(.footnote)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+                .rotationEffect(Angle(degrees: -90))
+            Spacer()
+        } //:VSTACK
+        .background(ColorGray.cornerRadius(12))
+        .frame(width: 85)
     }
 }
 
+// MARK: - PREVIEW<##>
+
 struct SectionView_Previews: PreviewProvider {
     static var previews: some View {
-        SectionView()
+        SectionView(rotateClockwise: false)
             .previewLayout(.fixed(width: 120, height: 240))
+            .padding()
+            .background(ColorBackground)
     }
 }
