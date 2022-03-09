90a2aaee0f8e8485d540534daf33d5ab39c3f1b7
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-09 17:56:57 +0600

    Update CategoryGridView.swift

--- 100DaysProjectSprint/ECommerceApp/ECommerceApp/Views/Home/CategoryGridView.swift
+++ 100DaysProjectSprint/ECommerceApp/ECommerceApp/Views/Home/CategoryGridView.swift
@@ -14,7 +14,10 @@ struct CategoryGridView: View {
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false, content: {
             LazyHGrid(rows: gridLayout, alignment: .center, spacing: columnSpacing, pinnedViews: [], content: {
-                Section(header: Text("HEADER"), footer: Text("FOOTER")) {
+                Section(
+                    header: SectionView(rotateClockwise: false),
+                    footer: SectionView(rotateClockwise: true)
+                ) {
                     ForEach(categories) { category in
                         CategoryItemView(category: category)
                     }
