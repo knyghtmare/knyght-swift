bf0e193ae716e27c668d9e9b2cc81daa5139ec13
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-16 21:28:45 +0600

    Update ContentView.swift

--- 100DaysProjectSprint/ECommerceApp/ECommerceApp/App/ContentView.swift
+++ 100DaysProjectSprint/ECommerceApp/ECommerceApp/App/ContentView.swift
@@ -34,6 +34,8 @@ struct ContentView: View {
                             .frame(height:230)
                         
                         CategoryGridView()
+                        
+                        TitleView(title: "Helmets")
 
                         FooterView()
                             .padding(.horizontal)
