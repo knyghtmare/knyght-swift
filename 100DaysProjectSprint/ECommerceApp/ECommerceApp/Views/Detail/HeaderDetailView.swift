dfe3b4146e02e9d89edddca0cad4821a57423f87
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-17 02:51:09 +0600

    Create HeaderDetailView.swift

---
+++ 100DaysProjectSprint/ECommerceApp/ECommerceApp/Views/Detail/HeaderDetailView.swift
@@ -0,0 +1,25 @@
+//
+//  HeaderDetailView.swift
+//  ECommerceApp
+//
+//  Created by Tahsin Jahin Khalid on 17/3/22.
+//
+
+import SwiftUI
+
+struct HeaderDetailView: View {
+    // MARK: - properties
+    
+    // MARK: - body<##><##>
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - preview<##>
+
+struct HeaderDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderDetailView()
+    }
+}
