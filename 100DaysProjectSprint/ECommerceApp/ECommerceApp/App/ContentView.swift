02a609d9e060b0f197df9b511c561fa46f21b872
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-16 22:17:36 +0600

    Update ContentView.swift

--- 100DaysProjectSprint/ECommerceApp/ECommerceApp/App/ContentView.swift
+++ 100DaysProjectSprint/ECommerceApp/ECommerceApp/App/ContentView.swift
@@ -36,6 +36,12 @@ struct ContentView: View {
                         CategoryGridView()
                         
                         TitleView(title: "Helmets")
+                        
+                        LazyVGrid(columns: gridLayout, spacing: 15, content: {
+                            ForEach(products) {product in
+                                ProductItemView(product: product)
+                            }
+                        }) //: GRID
 
                         FooterView()
                             .padding(.horizontal)
