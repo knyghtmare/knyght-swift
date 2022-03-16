aa433fd1f7e097b3827514a4b02341284fb5172c
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-17 01:26:58 +0600

    Create BrandGridView.swift

---
+++ 100DaysProjectSprint/ECommerceApp/ECommerceApp/Views/Home/BrandGridView.swift
@@ -0,0 +1,21 @@
+//
+//  BrandGridView.swift
+//  ECommerceApp
+//
+//  Created by Tahsin Jahin Khalid on 17/3/22.
+//
+
+import SwiftUI
+
+struct BrandGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BrandGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        BrandGridView()
+            .previewLayout(.sizeThatFits)
+    }
+}
