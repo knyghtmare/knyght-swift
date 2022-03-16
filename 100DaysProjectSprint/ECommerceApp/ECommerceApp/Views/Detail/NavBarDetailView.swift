4efad294eea352a9faf0024fe31c4b9b1d1e3bdb
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-17 02:46:33 +0600

    Create NavBarDetailView.swift

---
+++ 100DaysProjectSprint/ECommerceApp/ECommerceApp/Views/Detail/NavBarDetailView.swift
@@ -0,0 +1,28 @@
+//
+//  NavBarDetailView.swift
+//  ECommerceApp
+//
+//  Created by Tahsin Jahin Khalid on 17/3/22.
+//
+
+import SwiftUI
+
+struct NavBarDetailView: View {
+    // MARK: - properties
+    
+    // MARK: - body<##><##>
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - preview<##>
+
+struct NavBarDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavBarDetailView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+            .background(Color.gray)
+    }
+}
