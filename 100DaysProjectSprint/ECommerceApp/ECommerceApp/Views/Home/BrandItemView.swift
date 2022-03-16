99289d0f0c8e4500665f566a228680cf0608728b
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-17 00:25:58 +0600

    Create BrandItemView.swift

---
+++ 100DaysProjectSprint/ECommerceApp/ECommerceApp/Views/Home/BrandItemView.swift
@@ -0,0 +1,25 @@
+//
+//  BrandItemView.swift
+//  ECommerceApp
+//
+//  Created by Tahsin Jahin Khalid on 17/3/22.
+//
+
+import SwiftUI
+
+struct BrandItemView: View {
+    // MARK: - properties
+    
+    // MARK: - body<##><##>
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - preview<##>
+
+struct BrandItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        BrandItemView()
+    }
+}
