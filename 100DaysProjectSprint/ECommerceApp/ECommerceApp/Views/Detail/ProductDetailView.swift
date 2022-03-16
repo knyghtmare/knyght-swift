092b1cb08dfa1f2ba386f843d6482234e733e1fe
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-17 02:45:05 +0600

    Create ProductDetailView.swift

---
+++ 100DaysProjectSprint/ECommerceApp/ECommerceApp/Views/Detail/ProductDetailView.swift
@@ -0,0 +1,46 @@
+//
+//  SwiftUIView.swift
+//  ECommerceApp
+//
+//  Created by Tahsin Jahin Khalid on 17/3/22.
+//
+
+import SwiftUI
+
+struct ProductDetailView: View {
+    // MARK: - properties
+    
+    // MARK: - body<##><##>
+    var body: some View {
+        //Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 5, content: {
+            // NAVBAR
+            // HEADER
+            Text(sampleProduct.name)
+            // DETAIL TOP PART
+            // DETAIL BOTTOM PART
+            // RATINGS + SIZES
+            // DESCRIPTION
+            // QUANTITY + FAVOURITE
+            // ADD TO CART
+            Spacer()
+        }) //: VSTACK
+        .ignoresSafeArea(.all, edges: .all)
+        .background(
+            Color(
+                red: sampleProduct.redColor,
+                green: sampleProduct.greenColor,
+                blue: sampleProduct.blueColor
+            ).ignoresSafeArea(.all, edges: .all)
+        )
+    }
+}
+
+// MARK: - PREVIEW<##>
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductDetailView()
+            .previewLayout(.fixed(width: 375, height: 812))
+    }
+}
