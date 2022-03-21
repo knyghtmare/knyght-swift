0bd666b45df909be5a362cd584a314470774dfc2
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-21 21:15:03 +0600

    Update RatingsSizesView.swift

--- 100DaysProjectSprint/ECommerceApp/ECommerceApp/Views/Detail/RatingsSizesView.swift
+++ 100DaysProjectSprint/ECommerceApp/ECommerceApp/Views/Detail/RatingsSizesView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct RatingsSizesView: View {
     // MARK: - properties
     
+    // [String] means "a String array"
+    let sizes: [String] = ["XS", "S", "M", "L", "XL"]
+    
     // MARK: - body<##><##>
     var body: some View {
         HStack(alignment: .top, spacing: 3, content: {
@@ -41,10 +44,10 @@ struct RatingsSizesView: View {
                     .fontWeight(.semibold)
                     .foregroundColor(ColorGray)
                 
-                HStack(alignment: .center, spacing: 3, content: {
-                    ForEach(1...5, id: \.self) { item in
+                HStack(alignment: .center, spacing: 5, content: {
+                    ForEach(sizes, id: \.self) { size in
                         Button(action: {}, label: {
-                            Image(systemName: "star.fill")
+                            Text(size)
                         })
                             .frame(width: 28, height: 28, alignment: .center)
                             .background(ColorGray.cornerRadius(5))
