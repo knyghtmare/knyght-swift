f529047295c1c9d84d4f0e3956d7a62f3961d92b
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-21 23:42:46 +0600

    Update QuanFavView.swift

--- 100DaysProjectSprint/ECommerceApp/ECommerceApp/Views/Detail/QuanFavView.swift
+++ 100DaysProjectSprint/ECommerceApp/ECommerceApp/Views/Detail/QuanFavView.swift
@@ -16,7 +16,11 @@ struct QuanFavView: View {
     var body: some View {
         HStack(alignment: .center, spacing: 6, content: {
             // QUANTITY
-            Button(action: {}, label: {
+            Button(action: {
+                if counter > 0 {
+                    counter -= 1
+                }
+            }, label: {
                 Image(systemName: "minus.circle")
             }) //BUTTON
             
@@ -24,7 +28,11 @@ struct QuanFavView: View {
                 .fontWeight(.semibold)
                 .frame(minWidth: 36)
             
-            Button(action: {}, label: {
+            Button(action: {
+                if counter < 100 {
+                    counter += 1
+                }
+            }, label: {
                 Image(systemName: "plus.circle")
             }) //BUTTON
             
