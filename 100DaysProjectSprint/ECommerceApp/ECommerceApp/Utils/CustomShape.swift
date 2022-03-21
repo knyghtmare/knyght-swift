10c6e84a8fb12eae218e2076c68f3fe6ba24be49
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-21 20:31:24 +0600

    Update CustomShape.swift

--- 100DaysProjectSprint/ECommerceApp/ECommerceApp/Utils/CustomShape.swift
+++ 100DaysProjectSprint/ECommerceApp/ECommerceApp/Utils/CustomShape.swift
@@ -7,14 +7,18 @@
 
 import SwiftUI
 
-struct CustomShape: View {
-    var body: some View {
-        Text("Hello!")
+struct CustomShape: Shape {
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: [.topLeft, .topRight], cornerRadii: CGSize(width: 35, height: 35))
+        
+        return Path(path.cgPath)
     }
 }
 
 struct CustomShape_Preview: PreviewProvider {
     static var previews: some View {
         CustomShape()
+            .previewLayout(.fixed(width: 428, height: 120))
+            .padding()
     }
 }
