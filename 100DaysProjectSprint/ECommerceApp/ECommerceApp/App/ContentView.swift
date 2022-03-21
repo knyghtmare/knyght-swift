5733f3a7671ca3bada268ec20e2c7c92261a5101
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-22 01:54:51 +0600

    Update ContentView.swift

--- 100DaysProjectSprint/ECommerceApp/ECommerceApp/App/ContentView.swift
+++ 100DaysProjectSprint/ECommerceApp/ECommerceApp/App/ContentView.swift
@@ -16,46 +16,50 @@ struct ContentView: View {
     var body: some View {
         //Text("Hello, world!")
         ZStack {
-            VStack(spacing: 0) {
-                
-                NavBarView()
-                    .padding(.horizontal, 15)
-                    .padding(.bottom)
-                // accomodate for the notch
-                    .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
-                    .background(Color.white)
-                    .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
-                
-                ScrollView(.vertical, showsIndicators: false, content: {
-                    VStack(spacing: 0, content: {
-                        
-                        FeatureTabView()
-                            //.padding(.vertical, 20)
-                            // not sure why it decided not to scale
-                            // but adding the frame modifier patched it
-                            .frame(height:230)
-                        
-                        CategoryGridView()
-                        
-                        TitleView(title: "Helmets")
-                        
-                        LazyVGrid(columns: gridLayout, spacing: 15, content: {
-                            ForEach(products) {product in
-                                ProductItemView(product: product)
-                            } // :LOOP
-                        }) //: GRID
-                        .padding(15)
-                        
-                        TitleView(title: "Brands")
-                        
-                        BrandGridView()
+            if shop.showingProduct == false && shop.selectedProduct == nil {
+                VStack(spacing: 0) {
+                    
+                    NavBarView()
+                        .padding(.horizontal, 15)
+                        .padding(.bottom)
+                    // accomodate for the notch
+                        .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+                        .background(Color.white)
+                        .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
+                    
+                    ScrollView(.vertical, showsIndicators: false, content: {
+                        VStack(spacing: 0, content: {
+                            
+                            FeatureTabView()
+                                //.padding(.vertical, 20)
+                                // not sure why it decided not to scale
+                                // but adding the frame modifier patched it
+                                .frame(height:230)
+                            
+                            CategoryGridView()
+                            
+                            TitleView(title: "Helmets")
+                            
+                            LazyVGrid(columns: gridLayout, spacing: 15, content: {
+                                ForEach(products) {product in
+                                    ProductItemView(product: product)
+                                } // :LOOP
+                            }) //: GRID
+                            .padding(15)
+                            
+                            TitleView(title: "Brands")
+                            
+                            BrandGridView()
 
-                        FooterView()
-                            .padding(.horizontal)
-                    }) // VSTACK
-                }) // Scroll
-            } //: VSTACK
-            .background(ColorBackground.ignoresSafeArea(.all, edges: .all))
+                            FooterView()
+                                .padding(.horizontal)
+                        }) // VSTACK
+                    }) // Scroll
+                } //: VSTACK
+                .background(ColorBackground.ignoresSafeArea(.all, edges: .all))
+            } else {
+                /*@START_MENU_TOKEN@*/EmptyView()/*@END_MENU_TOKEN@*/
+            } // conditional
         } //: ZSTACK
         .ignoresSafeArea(.all, edges: .top)
     }
