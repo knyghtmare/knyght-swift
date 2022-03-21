348e86a4681084d8958043b7cdf678bf7fa7d2af
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-22 00:47:01 +0600

    Update AddKartView.swift

--- 100DaysProjectSprint/ECommerceApp/ECommerceApp/Views/Detail/AddKartView.swift
+++ 100DaysProjectSprint/ECommerceApp/ECommerceApp/Views/Detail/AddKartView.swift
@@ -8,13 +8,20 @@
 import SwiftUI
 
 struct AddKartView: View {
+    // MARK: - properties
+    
+    // MARK: - body<##><##>
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
+// MARK: - PREVIEW<##>
+
 struct AddKartView_Previews: PreviewProvider {
     static var previews: some View {
         AddKartView()
+            .previewLayout(.sizeThatFits)
+            .padding()
     }
 }
