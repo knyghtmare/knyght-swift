ee61d62f014c110fc370d99a600947bea8493a2c
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-21 20:51:44 +0600

    Create RatingsSizesView.swift

---
+++ 100DaysProjectSprint/ECommerceApp/ECommerceApp/Views/Detail/RatingsSizesView.swift
@@ -0,0 +1,25 @@
+//
+//  RatingsSizesView.swift
+//  ECommerceApp
+//
+//  Created by Tahsin Jahin Khalid on 21/3/22.
+//
+
+import SwiftUI
+
+struct RatingsSizesView: View {
+    // MARK: - properties
+    
+    // MARK: - body<##><##>
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW<##>
+
+struct RatingsSizesView_Previews: PreviewProvider {
+    static var previews: some View {
+        RatingsSizesView()
+    }
+}
