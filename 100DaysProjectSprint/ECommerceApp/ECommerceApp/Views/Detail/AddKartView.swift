00cada19e037a576a0662e0d5736e670c876270c
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-22 00:44:14 +0600

    Create AddKartView.swift

---
+++ 100DaysProjectSprint/ECommerceApp/ECommerceApp/Views/Detail/AddKartView.swift
@@ -0,0 +1,20 @@
+//
+//  AddKartView.swift
+//  ECommerceApp
+//
+//  Created by Tahsin Jahin Khalid on 22/3/22.
+//
+
+import SwiftUI
+
+struct AddKartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddKartView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddKartView()
+    }
+}
