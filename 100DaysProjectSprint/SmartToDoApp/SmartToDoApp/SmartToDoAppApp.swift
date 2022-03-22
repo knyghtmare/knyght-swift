4eae759f285afeddcd2ff7deb465a78665a0f59b
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-22 19:24:59 +0600

    started project 7

---
+++ 100DaysProjectSprint/SmartToDoApp/SmartToDoApp/SmartToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartToDoAppApp.swift
+//  SmartToDoApp
+//
+//  Created by Tahsin Jahin Khalid on 22/3/22.
+//
+
+import SwiftUI
+
+@main
+struct SmartToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
