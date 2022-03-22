2c6aa7163bc7f86222f42207213c007e3c47ae0f
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-22 20:19:48 +0600

    Create Item+CoreDataClass.swift

---
+++ 100DaysProjectSprint/SmartToDo/SmartToDo/Model/Item+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Item+CoreDataClass.swift
+//  SmartToDo
+//
+//  Created by Tahsin Jahin Khalid on 22/3/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Item)
+public class Item: NSManagedObject {
+
+}
