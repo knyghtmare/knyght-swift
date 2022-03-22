815a18a92ccda8172221ddaf6d9dadd047a014fd
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-03-22 22:39:16 +0600

    Update Constants.swift

--- 100DaysProjectSprint/SmartToDo/SmartToDo/Utils/Constants.swift
+++ 100DaysProjectSprint/SmartToDo/SmartToDo/Utils/Constants.swift
@@ -6,3 +6,16 @@
 //
 
 import SwiftUI
+
+// MARK: - FORMATTER
+
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+// MARK: - UX<##>
